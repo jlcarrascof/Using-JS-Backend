526b36612abc030a2556477960ecb4ea5e843664
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-10 17:59:55 -0400

    Refactor index.js to use http module for creating a server

--- 1.2.-First-Server-Node/index.js
+++ 1.2.-First-Server-Node/index.js
@@ -1,4 +1,8 @@
 const http = require('http');
-const server = http.createServer();
+const server = http.createServer(onRequest);
 server.listen(8000);
 console.log('Server is running on port 8000');
+
+function onRequest(request, response) {
+    console.log('Request received');
+}
