cf5546e8d877c3ac72466ca94245a9a819cced4f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-10 21:48:05 -0400

    Refactor index.js to log the request URL in onRequest function

--- 1.2.-First-Server-Node/index.js
+++ 1.2.-First-Server-Node/index.js
@@ -5,5 +5,5 @@ console.log('Server is running on port 8000');
 
 function onRequest(request, response) {
     console.log('Request received');
-    console.log(request.headers.host);
+    console.log(request.url);
 }
