597116b9a997964b96d58807f6e4057f36b70393
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-10 21:48:42 -0400

    Refactor index.js to log the request method in onRequest function

--- 1.2.-First-Server-Node/index.js
+++ 1.2.-First-Server-Node/index.js
@@ -5,5 +5,5 @@ console.log('Server is running on port 8000');
 
 function onRequest(request, response) {
     console.log('Request received');
-    console.log(request.url);
+    console.log(request.method);
 }
