ce91f22674bc1d0eda2d2da9015602c40e37a36d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-14 23:12:29 -0400

    refactor: Add CRUD routes for users in Express project

--- 1.3.-Express/src/routes/users.js
+++ 1.3.-Express/src/routes/users.js
@@ -5,4 +5,16 @@ router.get('/users', (req, res) => {
     res.send('Printing all users');
 })
 
+router.get('/create', (req, res) => {
+    res.send('Create users');
+})
+
+router.get('/update', (req, res) => {
+    res.send('Update users');
+})
+
+router.get('/delete', (req, res) => {
+    res.send('Delete users');
+})
+
 module.exports = router;
