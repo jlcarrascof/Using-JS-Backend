679039cd0f98c1764d73c5ac1bb025929ddf41e2
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-14 22:32:34 -0400

    refactor: Add route for '/users' in Express project

--- 1.3.-Express/src/index.js
+++ 1.3.-Express/src/index.js
@@ -8,10 +8,6 @@ app.get('/', (req, res) => {
     res.send('Welcome');
 })
 
-app.get('/users', (req, res) => {
-    res.send('Printing all users');
-})
-
 app.listen(port, () => {
     console.log("My app is running at port 3000");
 })
---
+++ 1.3.-Express/src/routes/users.js
@@ -0,0 +1,5 @@
+
+
+app.get('/users', (req, res) => {
+    res.send('Printing all users');
+})
