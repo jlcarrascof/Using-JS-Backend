0a90e83275a4c812618e957c9cb18027652404d0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-18 14:47:15 -0400

    refactor: Update Express routes and middleware for improved user CRUD operations and form handling

--- 1.3.-Express/src/index.js
+++ 1.3.-Express/src/index.js
@@ -14,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 // app.use(loggedMiddleware.isLogged);
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
 
 // Routes...
 
