adf5cb75acecfc90ee9699601d7b76cbe61a7aee
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-18 14:20:50 -0400

    refactor: Update Express routes for user CRUD operations and add create, update, and delete endpoints

--- 1.3.-Express/src/routes/users.js
+++ 1.3.-Express/src/routes/users.js
@@ -3,11 +3,12 @@ const router = express.Router()
 const userController = require('../controllers/users')
 
 router.get('/all', userController.getUsers)
-
 router.get('/create', userController.getCreateUsers)
-
 router.get('/update', userController.getUpdateUsers)
-
 router.get('/delete', userController.getDeleteUsers)
+
+router.post('/create', userController.createUser)
+router.put('/update', userController.updateUser)
+router.delete('/delete', userController.deleteUser)
 
 module.exports = router;
