e6bb8571bf7426557b55efc374667dc784fe5f42
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-25 22:29:27 -0400

    refactor: Update MySQL connection setup and error handling in connection.js

--- 1.4.-Databases-SQL/src/connection.js
+++ 1.4.-Databases-SQL/src/connection.js
@@ -1,2 +1,12 @@
 const mysql = require('mysql');
 const { mysql_database } = require('./config');
+
+const connection = mysql.createConnection(mysql_database);
+connection.connect((err, conn) => {
+    if (err) {
+        console.log('An error ocurred');
+    } else {
+        console.log('Connection succesful');
+        return conn;
+    }
+})
