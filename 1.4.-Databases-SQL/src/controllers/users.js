1d17549f62da9661c1513ff8d5d92a04aea178f5
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-26 13:22:32 -0400

    refactor: Update getUpdateUsers controller to retrieve user data from MySQL database

--- 1.4.-Databases-SQL/src/controllers/users.js
+++ 1.4.-Databases-SQL/src/controllers/users.js
@@ -19,8 +19,13 @@ const getCreateUsers = (req, res) => {
 const getUpdateUsers = (req, res) => {
     const param = req.params.id;
     const sql = 'SELECT * FROM users WHERE id = ?';
-    connection.query();
-    console.log(param);
+    connection.query(sql, param, (err, result) => {
+        if (err) {
+            console.log('An error ocurred: ', err);
+        } else {
+            console.log(result);
+        }
+    });
     res.render('update-user');
 }
 
