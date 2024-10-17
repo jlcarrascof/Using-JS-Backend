48ebc2478f637e4f2648c13aee5566aa49d53afd
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-16 22:39:32 -0400

    refactor: Update connection.js to use mongoose for MongoDB connection error handling

--- 1.5.-Databases-MongoDB/src/connection.js
+++ 1.5.-Databases-MongoDB/src/connection.js
@@ -3,6 +3,8 @@ const { mongodb } = require('./config');
 
 mongoose.connect(`mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`).then((db) => {
     console.log('Connection succesful');
+}).catch((err) => {
+    console.log('An error ocurred: ' + err);
 });
 
 module.exports = connection;
