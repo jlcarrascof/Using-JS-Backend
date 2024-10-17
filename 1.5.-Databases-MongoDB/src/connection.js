84c57a887848be85813a97e94fbf3d9b6283fd63
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-16 22:29:52 -0400

    refactor: Update connection.js to use mongoose for MongoDB connection

--- 1.5.-Databases-MongoDB/src/connection.js
+++ 1.5.-Databases-MongoDB/src/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 const { mongodb } = require('./config');
 
-mongoose.connect(`mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`).then(() => {
+mongoose.connect(`mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`).then((db) => {
     console.log('Connection succesful');
 });
-
-
 
 module.exports = connection;
